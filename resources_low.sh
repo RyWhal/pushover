73847cb1080f9f256d61f0b8d007b518d0049391
Ryan <RyWhal@users.noreply.github.com>
2014-04-24 17:58:13 -0700

    adding some comments

--- resources_low.sh
+++ resources_low.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #Author: Ryan Whalen
 
-MEMFREE=`free -m | awk '{print $4}' | sed -n 3p`
+#commands to run
+MEMFREE=`free -m | awk '{print $4}' | sed -n 2p`
 LOAD=`uptime | sed 's/.*load average: //' | awk -F\, '{print $3}'`
 DISK=`df -h | grep rootfs | awk '{print $2}' | egrep -o "[0-9]*"`
 
+#if memory free is less than 25 Mb then push a notification via "pushover"
 if [[ $MEMFREE -lt 25 ]]
 then
 	$(pushover -m "$(free -m)" -T "Server Memory Critical" -s $(date +%s))
 fi
 
+#if 15 minute  load Avg is greater than 1, push a notification via pushover
 if [[ $LOAD > 1.00 ]]
 then
 	$(pushover -m "$(uptime)" -T "Server Load Critical" -s $(date +%s))
 fi
 
+#if Disk space is lower than 2GB, push a notification via pushover
 if [[ $DISK < 2 ]]
 then
 	$(pushover -m "$(df -h)" -T "Disk Space Critical" -s $(date +%s))
