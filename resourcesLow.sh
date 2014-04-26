cd6dfecd38ea5301cc263c1d90f09f4a929d189b
Ryan <RyWhal@users.noreply.github.com>
2014-04-26 09:55:56 -0700

    Update resourcesLow.sh

--- resourcesLow.sh
+++ resourcesLow.sh
@@ -9,17 +9,17 @@ DISK=`df -h | grep rootfs | awk '{print $2}' | egrep -o "[0-9]*"`
 #if memory free is less than 25 Mb then push a notification via "pushover"
 if [[ $MEMFREE -lt 25 ]]
 then
-	$(pushover -m "$(free -m)" -T "Server Memory Critical" -s $(date +%s))
+	$(pushover.sh -m "$(free -m)" -T "Server Memory Critical" -s $(date +%s))
 fi
 
 #if 15 minute  load Avg is greater than 1, push a notification via pushover
 if [[ $LOAD > 1.00 ]]
 then
-	$(pushover -m "$(uptime)" -T "Server Load Critical" -s $(date +%s))
+	$(pushover.sh -m "$(uptime)" -T "Server Load Critical" -s $(date +%s))
 fi
 
 #if Disk space is lower than 2GB, push a notification via pushover
 if [[ $DISK < 2 ]]
 then
-	$(pushover -m "$(df -h)" -T "Disk Space Critical" -s $(date +%s))
+	$(pushover.sh -m "$(df -h)" -T "Disk Space Critical" -s $(date +%s))
 fi
