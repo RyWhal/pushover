5b3d560f440fcf3cee0639de97c013554be8ec48
RyWhal <ryanjpwhalen@gmail.com>
2014-05-05 23:03:56 +0000

    removed load checking from resourceslow.sh and added loadchecker.sh

---
+++ loadchecker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Author: Ryan Whalen
+#
+#This script checks for high load and messages me if its high and takes a log
+#of the current state of Top
+
+
+#Variable declerations:
+
+#get just the whole number of the 1min load avg. Cut off the decimal places
+LOADAVG=`uptime | egrep -o '[0-9]*\.[0-9]{2}' | head -1 | cut -f 1 -d .`
+SLEEP_TIME=10 #set sleep time
+START=`date +%s` #set start epoch time
+DIR="/var/log/loadchecker/LOG_$(date +%m%d_%H%M)" #the directory for TOP
+
+#main loop
+#if load is greater than 1; start the loop
+if [[ $LOADAVG -gt 1 ]];
+then
+	mkdir -p $DIR  #make a directory for the load alert
+
+	#push a notification to my phone about the alert
+	$(pushover -m "$(uptime)" -T "Load is getting high on $(hostname)" -s $(date +%s))
+
+	#while load is over 1 run this loop
+	while true; do
+
+		#check load again, if it recovers break from the loop and send me a notification
+		LOADAVG=`uptime | egrep -o '[0-9]*\.[0-9]{2}' | head -1 | cut -f 1 -d .`
+		if [[ $LOADAVG -lt 1 ]];
+		then
+			$(pushover -m "$(uptime)" -T "Load recovered on $(hostname)" -s $(date +%s))
+			exit
+		fi
+
+		CUR_TIME=$(date +%H%M%S) #current time to name the file
+		top -n1 -c -b > $DIR/top-$CUR_TIME #dump contents of a top to a file
+		sleep $SLEEP_TIME #sleep for 10 seconds and do the loop again.
+	done
+fi
+#end
+
--- resourcesLow.sh
+++ resourcesLow.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #Author: Ryan Whalen
+#runs on a cron and checks for low disk space or memory every 15 minutes
 
 #commands to run
 MEMFREE=`free -m | awk '{print $4}' | sed -n 2p`
-LOAD=`uptime | sed 's/.*load average: //' | awk -F\, '{print $3}'`
+#LOAD=`uptime | sed 's/.*load average: //' | awk -F\, '{print $3}'`
 DISK=`df -h | grep rootfs | awk '{print $2}' | egrep -o "[0-9]*"`
 
 #if memory free is less than 25 Mb then push a notification via "pushover"
@@ -12,11 +13,13 @@ then
 	$(pushover.sh -m "$(free -m)" -T "Server Memory Critical" -s $(date +%s))
 fi
 
+#commenting this section out since I'm adding a script specifically for load checking
+
 #if 15 minute  load Avg is greater than 1, push a notification via pushover
-if [[ $LOAD > 1.00 ]]
-then
-	$(pushover.sh -m "$(uptime)" -T "Server Load Critical" -s $(date +%s))
-fi
+#if [[ $LOAD > 1.00 ]]
+#then
+#	$(pushover.sh -m "$(uptime)" -T "Server Load Critical" -s $(date +%s))
+#fi
 
 #if Disk space is lower than 2GB, push a notification via pushover
 if [[ $DISK < 2 ]]
